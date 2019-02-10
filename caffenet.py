01455cb1f4ada70486aecac3732c957f0bbf3e96
mleclair73 <malcolmleclair@gmail.com>
2019-02-10 15:23:13 -0500

    bugfix

--- caffenet.py
+++ caffenet.py
@@ -379,9 +379,10 @@ class Interpolate(nn.Module):
         self.interp = nn.functional.interpolate
         self.size = size
         self.mode = mode
+        self.align_corners = align_corners
         
     def forward(self, x):
-        x = self.interp(x, size=self.size, mode=self.mode, align_corners=align_corners)
+        x = self.interp(x, size=self.size, mode=self.mode, align_corners=self.align_corners)
         return x
 
 class CaffeNet(nn.Module):
