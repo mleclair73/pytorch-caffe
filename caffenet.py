8cc5b5521e2106ae1f6922699ccb0ffabca119a8
mleclair73 <malcolmleclair@gmail.com>
2019-02-10 16:05:07 -0500

    bigfix

--- caffenet.py
+++ caffenet.py
@@ -1010,9 +1010,10 @@ class CaffeNet(nn.Module):
                     scale = int(params['shrink_factor'])
                 if 'zoom_factor' in params:
                     scale = int(params['zoom_factor'])
+                if scale:
+                    blob_width[tname] = int(math.floor(input_width * scale))
+                    blob_height[tname] = int(math.floor(input_height * scale))
                 
-                blob_width[tname] = int(math.floor(input_width * scale))
-                blob_height[tname] = int(math.floor(input_height * scale))
 
                 if 'height' in params and 'weight' in params:
                     size = (int(layer['interp_param']['height']),
